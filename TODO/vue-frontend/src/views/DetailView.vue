ea8f2c9a36dcb0dacc9c163536bfe52b745b4932
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-05-02 19:29:40 +0500

    Update DetailView.vue

--- TODO/vue-frontend/src/views/DetailView.vue
+++ TODO/vue-frontend/src/views/DetailView.vue
@@ -19,6 +19,9 @@ export default {
     data() {
         return {
             todo: [],
+            form: {
+                body: '',
+            }
         };
     },
 
@@ -38,6 +41,7 @@ export default {
             await axios.get(`/todo/detail/${this.$route.params.id}/`, config)
                 .then(response => {
                     this.todo = response.data
+                    this.form.body = this.todo.body
                 })
                 .catch(error => {
                     console.log(error)
@@ -46,6 +50,37 @@ export default {
         logout() {
             this.userStore.removeToken()
             location.assign('/login')
+        },
+        async updateTodo() {
+            const config = {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('user.access')}`,
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            };
+
+            const updateData = {
+                name: this.todo.name,
+                body: this.form.body
+            };
+
+            try {
+                const response = await axios.put(`/todo/detail/${this.$route.params.id}/`, updateData, config);
+                this.todo = response.data;
+                this.$router.push('/');
+            } catch (error) {
+                if (error.response && error.response.status === 400) {
+                    const validationErrors = error.response.data;
+                    console.error('Validation Errors:', validationErrors);
+                } else {
+                    console.error('Server Error:', error.response.status, error.response.data);
+                }
+            }
+
+        },
+        submit() {
+            this.updateTodo();
         },
     },
 
@@ -63,7 +98,7 @@ export default {
                     <address class="flex items-center mb-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                             <div class="mr-6 rounded-full">
-                                <RouterLink :to="{ name: 'home' }"
+                                <a @click="submit"
                                     class="text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700">
                                     <span
                                         class="text-xs inline-flex bg-primary-600 rounded-full text-white px-5 py-1.5">
@@ -74,7 +109,7 @@ export default {
                                                 stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4" />
                                         </svg>
                                     </span>
-                                </RouterLink>
+                                </a>
                             </div>
                             <div>
                                 <a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">
@@ -91,13 +126,19 @@ export default {
                             </div>
                         </div>
                     </address>
-                    <h1
-                        class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
-                        {{ todo.name }}
-                    </h1>
                 </header>
                 <p class="lead">
-                <div v-html="todo.body"></div>
+                <div class="sm:col-span-2">
+                    <label for="description" class="block mb-2 text-sm font-medium text-gray-900">
+                        <h1
+                            class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                            {{ todo.name }}
+                        </h1>
+                    </label>
+                    <textarea id="description" rows="8"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-50"
+                        v-model="form.body"></textarea>
+                </div>
                 </p>
             </article>
         </div>
